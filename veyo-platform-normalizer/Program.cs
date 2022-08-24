45e0f97bffd8f2409e1dfe80065be7dfcef55396
Adam R Grey <agrey@veyo.com>
2022-08-24 11:32:31 -0700

    [WIP] parsing XML, as normal - but it breaks the file atm

---
+++ veyo-platform-normalizer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var workingDir = args?.FirstOrDefault() ?? Directory.GetCurrentDirectory();
+        var foundProjs =
+            Directory.GetFiles(workingDir, "*.csproj", SearchOption.AllDirectories);  /** test todo:
+                                                                                        * csproj in subdir: ?
+                                                                                        * csproj in top level dir: ?
+                                                                                        * csproj in both: ?
+                                                                                        * multiple csprojs ostensibly owning that tree: ?
+                                                                                        *    so like, projs/1.csproj, projs/2.csproj, projs/helpers/3.csproj; we can do stuff but they can't all own the subdirectories
+                                                                                        * no csprojs found: ?
+                                                                                        * args[0] present but invalid: ?
+                                                                                        **/
+        foreach (var foundProjsFile in foundProjs)
+        {
+            var partialPath = Path.GetRelativePath(workingDir, foundProjsFile);
+            var problemFound = false;
+            XElement doc;
+            try
+            {
+                doc = XElement.Load(foundProjsFile);
+            }
+            catch (System.Xml.XmlException e)
+            {
+                Console.Error.WriteLine("error loading " + foundProjsFile);
+                Console.Error.WriteLine(e.Message);
+                continue;
+            }
+            var referencedFiles = doc.Descendants().Where(xe => xe.Name.LocalName == "Compile");
+            if (referencedFiles.Count() > referencedFiles.Select(xe => xe.Attribute("Include")?.Value).Distinct().Count())
+            {
+
+                Console.WriteLine($"!! {partialPath} !!");
+                problemFound = true;
+                Console.WriteLine("duplicates found!");
+
+                foreach (var refd in referencedFiles.ToList())
+                {
+                    if(referencedFiles.Count(xe => xe.Attribute("Include")?.Value == refd.Attribute("Include")?.Value) > 1)
+                    {
+                        Console.WriteLine($"removing {refd.Attribute("Include")?.Value}");
+                        refd.Remove();
+                    }
+                }
+            }
+
+            foreach (var refd in referencedFiles.ToList())
+            {
+                var file = refd.Attribute("Include")?.Value;
+                if (!string.IsNullOrWhiteSpace(file) && !File.Exists(file))
+                {
+                    refd.Remove();
+                    if (!problemFound)
+                    {
+                        Console.WriteLine($"!! {partialPath} !!");
+                        problemFound = true;
+                    }
+
+                    Console.WriteLine($"{file} missing, reference removed");
+                }
+            }
+
+            //TODO: add references to missing files
+
+            //TODO: merge to one big itemgroup
+
+            //still searchin throuh multiple itemgroups, in case they have attributes
+            foreach (var group in doc.Descendants().Where(xe => xe.Name.LocalName == "ItemGroup"))
+            {
+                var compileItems = group.Descendants().Where(xe => xe.Name.LocalName == "Compile");
+                if(!compileItems.Select(xe => xe.Attribute("Include")?.Value).SequenceEqual(compileItems.OrderBy(ci => ci.Attribute("Include")?.Value).Select(xe => xe.Attribute("Include")?.Value)))
+                {
+                    if (!problemFound)
+                    {
+                        Console.WriteLine($"!! {partialPath} !!");
+                        problemFound = true;
+                    }
+
+                    Console.WriteLine($"sorting an itemgroup");
+                    var compileItmesCopy = compileItems.OrderBy(ci => ci.Attribute("Include")?.Value).ToList();
+                    var noncompileItems = group.Descendants().Where(xe => xe.Name.LocalName != "Compile").ToList();
+                    group.RemoveAll();
+                    group.Add(noncompileItems);
+                    group.Add(compileItmesCopy);
+                }
+            }
+
+            if (!problemFound)
+            {
+                Console.WriteLine($"{partialPath} fine");
+            }
+            else
+            {
+                doc.Save(foundProjsFile);
+            }
+        }
+    }
+
+}
